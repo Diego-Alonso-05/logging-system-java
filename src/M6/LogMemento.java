cb4d52032ae162172bb016f3e0b718da71655b6c
Diego-Alonso-05 <pv33986@alunos.estgv.ipv.pt>
2026-03-26 16:43:48 +0000

    LogMemento fixed and functional, stored state of log tree correct

--- src/M6/LogMemento.java
+++ src/M6/LogMemento.java
@@ -1,15 +1,18 @@
 package M6;
 
 import M1.LogConfig;
+import M4.LogGroup;
 
 public class LogMemento {
 
     private final LogConfig config;
     private final String destinationType;
+    private final LogGroup root;
 
-    public LogMemento(LogConfig config, String destinationType) {
+    public LogMemento(LogConfig config, String destinationType, LogGroup root) {
         this.config = config;
         this.destinationType = destinationType;
+        this.root = root;
     }
 
     public LogConfig getConfig() {
@@ -18,5 +21,9 @@ public class LogMemento {
 
     public String getDestinationType() {
         return destinationType;
+    }
+
+    public LogGroup getRoot() {
+        return root;
     }
 }
